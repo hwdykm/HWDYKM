ea42b8d349628a73f79f7196d87af81e9ac6ec98
eltim12 <eltim2000@gmail.com>
2019-03-21 19:11:55 +0700

    start project

---
+++ client/src/api/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+var config = {
+  apiKey: "AIzaSyAVvZQ9AcODCM24mwKqXhRxxWq2xyWkA4Q",
+  authDomain: "how-well-do-you-know-me-aa191.firebaseapp.com",
+  databaseURL: "https://how-well-do-you-know-me-aa191.firebaseio.com",
+  projectId: "how-well-do-you-know-me-aa191",
+  storageBucket: "how-well-do-you-know-me-aa191.appspot.com",
+  messagingSenderId: "451978634728"
+};
+
+firebase.initializeApp(config);
+
+const db = firebase.firestore();
+
+export default db;
