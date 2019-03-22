3651ae3f41eaf8dd580ff843c7002dcf4f2e3208
eltim12 <eltim2000@gmail.com>
2019-03-22 11:25:52 +0700

    get roomname done

--- client/src/views/PlayRoom.vue
+++ client/src/views/PlayRoom.vue
@@ -1,19 +1,31 @@
 <template>
-    <div>
-        <h1>helo from play room</h1>
+  <div class="container center">
+    <div class="continer">
+      <h1>play room id: {{ roomName }}</h1>
+      <a id="show-id-button" @click="toggle" class="btn modal-trigger waves-effect black">my room id</a>
     </div>
+    <h5 v-if="toggled">{{ roomName }}</h5>
+  </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
 
-
 export default {
-    name: 'playroom',
-    
-}
+  name: "playroom",
+  computed: mapState(["roomName", "player1", "player2", "roomStatus"]),
+  data() {
+    return {
+        toggled: false,
+    };
+  },
+  methods: {
+      toggle() {
+          this.toggled = !this.toggled
+      }
+  }
+};
 </script>
 
 <style>
-
 </style>
